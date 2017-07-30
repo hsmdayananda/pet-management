0e321d23233145b41b063fe941f0d85c66a477b3
Hasini <hsmdayananda@gmail.com>
2017-07-30 21:54:12 +0530

    Add files via upload
    inserting values to the db - working

---
+++ Owner.java
@@ -0,0 +1,99 @@
+package com.virtusa.petManagement.model;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "OWNER")
+public class Owner implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name="ownerName")
+    private String ownerName;
+
+    @Column(name="ownerMobile")
+    private String ownerMobile;
+
+    @Column(name="ownerCity")
+    private String ownerCity;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "id")
+    @JsonProperty("petsInformation")
+    private List<Pet> petData = new ArrayList<Pet>();
+
+    public List<Pet> getPetData() {
+        return petData;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "id=" + id +
+                ", ownerName='" + ownerName + '\'' +
+                ", ownerMobile='" + ownerMobile + '\'' +
+                ", ownerCity='" + ownerCity + '\'' +
+                ", petData=" + petData.toString() +
+                '}';
+    }
+
+
+    public void setPetData(List<Pet> petData) {
+
+
+        for(Pet pet:petData){
+            pet.setOwner(this);
+            System.out.println("pet " + petData);
+        }
+
+        this.petData = petData;
+        System.out.println("pet " + petData);
+    }
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerCity() {
+        return ownerCity;
+    }
+
+    public void setOwnerCity(String ownerCity) {
+        this.ownerCity = ownerCity;
+    }
+
+    public String getOwnerMobile() {
+        return ownerMobile;
+    }
+
+    public void setOwnerMobile(String ownerMobile) {
+        this.ownerMobile = ownerMobile;
+    }
+}
---
+++ Pet.java
@@ -0,0 +1,69 @@
+package com.virtusa.petManagement.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "petsInformation")
+public class Pet implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="pet_id")
+    private int id;
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+
+    @Column(name="petName")
+    private String name;
+
+    @Column(name="petType")
+    private String type;
+
+
+    @ManyToOne
+
+    private Owner owner;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
